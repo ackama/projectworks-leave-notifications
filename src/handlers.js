ec19298c25c70e193df3fc1768c0fa65181318cf
Josh McArthur <joshua.mcarthur@gmail.com>
2020-11-05 15:52:57 +1300

    Move leave formatting to handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,14 +1,57 @@
+const { format: formatDate } = require('date-fns');
 const { utcWeekRange, utcDayRange } = require('./dateRange');
-const { formattedLeavesBetween } = require('./leaves');
+const { leavesBetween } = require('./leaves');
+const { Notifier } = require('./notifier');
+
+const formatWeeklyLeaveForSlack = ({ user, days }) => {
+  const formattedDays = days
+    .map(
+      ({ Date: date, Hours: hours }) =>
+        `${formatDate(Date.parse(date), 'EEEE d LLL')}, ${hours} hrs`
+    )
+    .join(', ');
+
+  return `* ${user.FirstName} ${user.LastName}: ${formattedDays}`;
+};
+
+const formatDailyLeaveForSlack = ({ user, days }) => {
+  const formattedHours = days.map(({ Hours }) => `${Hours} hrs`).join(', ');
+
+  return `* ${user.FirstName} ${user.LastName}: ${formattedHours}`;
+};
 
 module.exports.weeklyReport = async () => {
-  const [startDate, endDate] = utcWeekRange();
+  const date = new Date();
+  const leaves = await leavesBetween(...utcWeekRange(date));
+  const notifier = new Notifier();
 
-  return formattedLeavesBetween(startDate, endDate);
+  if (leaves.length) {
+    notifier.bufferMessage('On leave this week:');
+    leaves.forEach(leave =>
+      notifier.bufferMessage(formatWeeklyLeaveForSlack(leave))
+    );
+  } else {
+    notifier.bufferMessage('No leave booked this week.');
+  }
+
+  return notifier.sendBufferedMessages();
 };
 
 module.exports.dailyReport = async () => {
-  const [startDate, endDate] = utcDayRange();
+  const date = new Date();
+  const leaves = await leavesBetween(...utcDayRange(date));
+  const notifier = new Notifier();
 
-  return formattedLeavesBetween(startDate, endDate);
+  if (leaves.length) {
+    notifier.bufferMessage(
+      `On leave today, ${formatDate(date, 'EEEE d LLL')}:`
+    );
+    leaves.forEach(leave =>
+      notifier.bufferMessage(formatDailyLeaveForSlack(leave))
+    );
+  } else {
+    notifier.bufferMessage('No leave booked today.');
+  }
+
+  return notifier.sendBufferedMessages();
 };
--- src/leaves.js
+++ src/leaves.js
@@ -1,39 +1,24 @@
-const dateFns = require('date-fns');
+const { isWithinInterval } = require('date-fns');
 const { fetchLeaves, fetchUser } = require('./projectworks');
 const APPROVED_LEAVE_STATUS = 2;
 
-module.exports.formattedLeavesBetween = async (startDate, endDate) => {
+module.exports.leavesBetween = async (startDate, endDate) => {
   const leaves = await fetchLeaves({
-    startDate,
-    endDate,
+    startDate: startDate.toUTCString(),
+    endDate: endDate.toUTCString(),
     statusId: APPROVED_LEAVE_STATUS,
     pageSize: 1000
   }).then(r => r.json());
 
-  let message = '';
-
-  // Not using forEach here because it's not async/await aware.
-  for (let index = 0; index < leaves.length; index++) {
-    const leave = leaves[index];
-
-    // ESLint would have us do this with a Promise.all() but procedurally we are
-    // looking up the user to use for message building.
-    // eslint-disable-next-line no-await-in-loop
-    const user = await fetchUser(leave.UserID);
-    const formattedDays = leave.Days.filter(d =>
-      dateFns.isWithinInterval(Date.parse(d.Date), {
-        start: Date.parse(startDate),
-        end: Date.parse(endDate)
-      })
-    )
-      .map(
-        ({ Date: date, Hours: hours }) =>
-          `${dateFns.format(Date.parse(date), 'EEEE d LLL')}, ${hours} hrs`
+  return Promise.all(
+    leaves.map(async leave => ({
+      user: await fetchUser(leave.UserID),
+      days: leave.Days.filter(({ Date: date }) =>
+        isWithinInterval(Date.parse(date), {
+          start: startDate,
+          end: endDate
+        })
       )
-      .join(', ');
-
-    message += `${user.FirstName} ${user.LastName}: ${formattedDays}\n`;
-  }
-
-  return message;
+    }))
+  );
 };
