c7015415cbf075ba4f4aba45b8863efb8c2b3d44
Eoin Kelly <eoin.kelly@ackama.com>
2022-09-23 07:58:37 +1200

    Improve slack message formatting again. (#6)
    This is a minor refactor and improves (I think) the formatting of the
    output slack messages. Daily messages are more compact now.

--- src/leaves.js
+++ src/leaves.js
@@ -4,24 +4,31 @@ const { sortByUser } = require('./sortByUsers');
 const APPROVED_LEAVE_STATUS = 2;
 
 module.exports.leavesBetween = async (startDate, endDate) => {
-  const leaves = await fetchLeaves({
+  const rawLeavesFromProjectWorks = await fetchLeaves({
     startDate: startDate.toUTCString(),
     endDate: endDate.toUTCString(),
     statusId: APPROVED_LEAVE_STATUS,
     pageSize: 1000
   }).then(r => r.json());
 
-  return sortByUser(
-    await Promise.all(
-      leaves.map(async leave => ({
-        user: await fetchUser(leave.UserID),
-        days: leave.Days.filter(({ Date: date }) =>
-          isWithinInterval(Date.parse(date), {
-            start: startDate,
-            end: endDate
-          })
-        )
-      }))
-    )
+  const decoratedLeaves = await Promise.all(
+    rawLeavesFromProjectWorks.map(async leave => ({
+      user: await fetchUser(leave.UserID),
+      days: leave.Days.filter(({ Date: date }) =>
+        isWithinInterval(Date.parse(date), {
+          start: startDate,
+          end: endDate
+        })
+      )
+    }))
   );
+
+  // decoratedLeaves sometimes includes leaves which don't actually have any
+  // days because the NZ day spans two UTC days. We cannot display anything
+  // meaningful about such leaves so we filter them out.
+  const decoratedLeavesWithDays = decoratedLeaves.filter(
+    leave => leave.days.length > 0
+  );
+
+  return sortByUser(decoratedLeavesWithDays);
 };
