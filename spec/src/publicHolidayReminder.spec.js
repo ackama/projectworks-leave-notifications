9e11caf5b76b99cd3893e46afafc05447c911135
Eoin Kelly <eoin@eoinkelly.info>
2022-09-25 11:38:14 +1300

    Add specs for public holiday reminders

---
+++ spec/src/publicHolidayReminder.spec.js
@@ -0,0 +1,136 @@
+const { DateTime } = require('luxon');
+const { generateDailyReport } = require('../../src/publicHolidayReminder');
+
+describe('generateDailyReport', () => {
+  const nzFixtureCalendarPath = './spec/fixtures/nz-public-holiday-dates.ics';
+  const auFixtureCalendarPath =
+    './spec/fixtures/au-victorian-public-holiday-dates.ics';
+  const mockNotifier = {
+    bufferMessage: jest.fn()
+  };
+
+  it('buffers expected messages a holiday in both NZ and AU (Christmas day)', async () => {
+    const xmasDay = DateTime.fromISO('2023-12-25', {
+      zone: 'Pacific/Auckland'
+    })
+      .toUTC()
+      .toJSDate();
+
+    await generateDailyReport(mockNotifier, {
+      startOfTodayInUTC: xmasDay,
+      nzCalendarUrl: nzFixtureCalendarPath,
+      auCalendarUrl: auFixtureCalendarPath
+    });
+
+    expect(mockNotifier.bufferMessage).toMatchInlineSnapshot(`
+    [MockFunction] {
+      "calls": [
+        [
+          "*Public holidays (beta):*",
+          "section",
+        ],
+        [
+          ":flag-nz: New Zealand: Public holiday (Christmas Day)",
+          "section",
+        ],
+        [
+          ":flag-au: Australia: Restricted trading day - only exempt shops are permitted to open (Christmas Day)",
+          "section",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+        {
+          "type": "return",
+          "value": undefined,
+        },
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+    `);
+  });
+
+  it('buffers expected messages a holiday in NZ only', async () => {
+    const waitangiDay = DateTime.fromISO('2023-02-06', {
+      zone: 'Pacific/Auckland'
+    })
+      .toUTC()
+      .toJSDate();
+
+    await generateDailyReport(mockNotifier, {
+      startOfTodayInUTC: waitangiDay,
+      nzCalendarUrl: nzFixtureCalendarPath,
+      auCalendarUrl: auFixtureCalendarPath
+    });
+
+    expect(mockNotifier.bufferMessage).toMatchInlineSnapshot(`
+    [MockFunction] {
+      "calls": [
+        [
+          "*Public holidays (beta):*",
+          "section",
+        ],
+        [
+          ":flag-nz: New Zealand: Public holiday (Waitangi Day)",
+          "section",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+    `);
+  });
+
+  it('buffers expected messages a holiday in AU only', async () => {
+    const melbourneCupDay = DateTime.fromISO('2023-11-07', {
+      zone: 'Pacific/Auckland'
+    })
+      .toUTC()
+      .toJSDate();
+
+    await generateDailyReport(mockNotifier, {
+      startOfTodayInUTC: melbourneCupDay,
+      nzCalendarUrl: nzFixtureCalendarPath,
+      auCalendarUrl: auFixtureCalendarPath
+    });
+
+    expect(mockNotifier.bufferMessage).toMatchInlineSnapshot(`
+    [MockFunction] {
+      "calls": [
+        [
+          "*Public holidays (beta):*",
+          "section",
+        ],
+        [
+          ":flag-au: Australia: Melbourne Cup Day is a public holiday across all of Victoria unless alternate local holiday has been arranged by non-metro council (Melbourne Cup)",
+          "section",
+        ],
+      ],
+      "results": [
+        {
+          "type": "return",
+          "value": undefined,
+        },
+        {
+          "type": "return",
+          "value": undefined,
+        },
+      ],
+    }
+    `);
+  });
+});
--- src/publicHolidayReminder.js
+++ src/publicHolidayReminder.js
@@ -1,17 +1,17 @@
 const { DateTime } = require('luxon');
 const ical = require('node-ical');
 
-const nzCalendarUrl =
+const NZ_CALENDAR_URL =
   'https://calendar.google.com/calendar/ical/en.new_zealand%23holiday%40group.v.calendar.google.com/public/basic.ics';
 
 // We have two options for the AU calendar. We are trying the Victoria gov one
 // first because it will (presumably) have more accurate data for Melbourne and
 // will avoid us having to filter out holidays from other states.
-// const auCalendarUrl = 'https://calendar.google.com/calendar/ical/en.australian.official%23holiday%40group.v.calendar.google.com/public/basic.ics'
-const auCalendarUrl =
+// const AU_CALENDAR_URL = 'https://calendar.google.com/calendar/ical/en.australian.official%23holiday%40group.v.calendar.google.com/public/basic.ics'
+const AU_CALENDAR_URL =
   'https://www.vic.gov.au/sites/default/files/2022-09/Victorian-public-holiday-dates.ics';
 
-const findEventsForDate = (rawEvents, date) => {
+const findRelevantEvents = (rawEvents, date) => {
   return Object.values(rawEvents)
     .filter(rawEvent => rawEvent.start?.toString() === date.toString())
     .map(rawEvent => {
@@ -22,14 +22,37 @@ const findEventsForDate = (rawEvents, date) => {
     });
 };
 
-module.exports.generateDailyReport = async notifier => {
-  const nzPublicHolidays = await ical.async.fromURL(nzCalendarUrl);
-  const auPublicHolidays = await ical.async.fromURL(auCalendarUrl);
+const getCalendarData = async filePathOrUrl => {
+  let events = [];
 
-  const startOfTodayInUTC = DateTime.now().startOf('day').toUTC().toJSDate();
+  if (filePathOrUrl.startsWith('http')) {
+    events = await ical.async.fromURL(filePathOrUrl);
+  } else {
+    events = await ical.async.parseFile(filePathOrUrl);
+  }
 
-  const nzEvents = findEventsForDate(nzPublicHolidays, startOfTodayInUTC);
-  const auEvents = findEventsForDate(auPublicHolidays, startOfTodayInUTC);
+  return events;
+};
+
+const getStartOfTodayInUTC = () => {
+  return DateTime.local({ zone: 'Pacific/Auckland' })
+    .startOf('day')
+    .toUTC()
+    .toJSDate();
+};
+
+module.exports.generateDailyReport = async (
+  notifier,
+  {
+    startOfTodayInUTC = getStartOfTodayInUTC(),
+    nzCalendarUrl = NZ_CALENDAR_URL,
+    auCalendarUrl = AU_CALENDAR_URL
+  } = {}
+) => {
+  const nzPublicHolidays = await getCalendarData(nzCalendarUrl);
+  const auPublicHolidays = await getCalendarData(auCalendarUrl);
+  const nzEvents = findRelevantEvents(nzPublicHolidays, startOfTodayInUTC);
+  const auEvents = findRelevantEvents(auPublicHolidays, startOfTodayInUTC);
 
   if (nzEvents.length || auEvents.length) {
     notifier.bufferMessage(`*Public holidays (beta):*`, 'section');
