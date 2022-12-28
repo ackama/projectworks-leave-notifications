0c4725615f4949a125b51edc63908ecee2b96f61
Eoin Kelly <eoin@eoinkelly.info>
2022-12-28 16:14:34 +1300

    Add AWS lambda types to handler functions

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,23 +1,28 @@
+import { Context, ScheduledEvent } from 'aws-lambda';
 import * as daylightSavingsChangeReminder from './daylightSavingsChangeReminder';
 import * as leaveReminder from './leaveReminder';
 import { Notifier } from './notifier';
 import * as publicHolidayReminder from './publicHolidayReminder';
-import type { NotifierResult } from './types';
 
-export const weeklyReport = async (): Promise<NotifierResult> => {
+export const weeklyReport = async (
+  _event: ScheduledEvent,
+  _context: Context
+): Promise<void> => {
   const notifier = new Notifier();
 
   await leaveReminder.generateWeeklyReport(notifier);
-
-  return notifier.sendBufferedMessages();
+  await notifier.sendBufferedMessages();
 };
 
-export const dailyReport = async (): Promise<NotifierResult> => {
+export const dailyReport = async (
+  _event: ScheduledEvent,
+  _context: Context
+): Promise<void> => {
   const notifier = new Notifier();
 
   await leaveReminder.generateDailyReport(notifier);
   await publicHolidayReminder.generateDailyReport(notifier);
   daylightSavingsChangeReminder.generateDailyReport(notifier);
 
-  return notifier.sendBufferedMessages();
+  await notifier.sendBufferedMessages();
 };
